54d9920084a4bb3014cf102cea2d1651947253b9
Arsahd Ali <arshad.ali@conurets.com>
2023-10-11 16:30:07 +0500

    basic setup with required dependancies.

---
+++ src/main/java/com/memon/arshad/KafakSpringBootHandsonApplication.java
@@ -0,0 +1,13 @@
+package com.memon.arshad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafakSpringBootHandsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafakSpringBootHandsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memon/arshad/KafakSpringBootHandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memon.arshad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafakSpringBootHandsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
